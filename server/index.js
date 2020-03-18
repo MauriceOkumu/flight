4120bcd825849ff2482d5c680741ca8f9a18f493
mauriceokumu <maurice.okumu@thoughtworks.com>
2020-03-18 15:47:48 -0500

    Add express server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// import express from 'express'
+const express = require('express')
+ const port  = 4000
+const server = express()
+
+server.get('/', (req, res) => {
+    res.send('Hello world')
+})
+server.listen(port, () => {
+    console.log('Server is listening on port ', port)
+})
