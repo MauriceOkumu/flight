6ca7d644617587b47a6e4b4641c9320c742f114a
Maurice <maurice@rlg.local>
2020-04-14 16:28:50 -0500

    Getting data from backend to frontend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import DB  from '../backend/database/index.js'
+
+dotenv.config()
+
 const port  = 4000
 const server = express()
 
-server.get('/', (req, res) => {
-    res.send('Hello world')
+const router = express.Router()
+
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+server.use(cors());
+server.use((req, res, next) => {
+    next()
 })
+
+
+const getFlights = () => {
+    return new Promise(function(resolve, reject) {
+    DB().query('SELECT * FROM flighter', (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows);
+      })
+    }) 
+  }
+
+
+router.get('/', async (req, res) => {
+  await getFlights().then((resl) => {
+    console.log('Rsults', resl)
+    res.send(resl)
+   })
+})
+
+server.use('/', router)
 server.listen(port, () => {
     console.log('Server is listening on port ', port)
 })
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React , { useState, useEffect} from 'react'
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
+  const [flights, setFlights] = useState('Hello')
+  useEffect(() => {
+    getFlights()
+  }, [])
+  function getFlights () {
+    // axios.get('/')
+    axios.get('http://localhost:4000/')
+    .then(res => {
+      console.log('Res => ', res.data)
+      setFlights(JSON.stringify(res.data))
+      
+    })
+  
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header >
     
           Flight Tracker
-          Awesome Sulu
        
       </header>
+      <div>
+        { flights }
+      </div>
     </div>
   )
 }
