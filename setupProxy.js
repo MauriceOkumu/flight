d5bfcc8375b8aff92f646767abd9a6f68c7674e5
Maurice <maurice@rlg.local>
2020-05-04 18:56:52 -0500

    Add

--- setupProxy.js
+++ setupProxy.js
@@ -2,6 +2,6 @@ import  proxy from "http-proxy-middleware"
 
 export default  function(app) {
   app.use(
-    proxy(["/api", , "/otherApi"], { target: "http://localhost:4000" })
+    proxy(["/api", , "/otherApi"], { target: "http://localhost:5000" })
   );
 };
